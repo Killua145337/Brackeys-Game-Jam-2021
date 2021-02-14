27d10e1b07182e7a5cd99aa0801cbb3c1756bf05
Killua145337 <68349080+Killua145337@users.noreply.github.com>
2021-02-14 21:10:19 +0330

    Changed Movement.cs names

--- Assets/Scripts - for programmers/Movement.cs
+++ Assets/Scripts - for programmers/Movement.cs
@@ -5,53 +5,53 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    [SerializeField] private float _movespeed;
-    [SerializeField] private float _JumpForce;
+    [SerializeField]  float moveSpeed;
+    [SerializeField]  float jumpForce;
 
-    Vector2 _Input;
-    Vector3 _moveVelocity;
-    Vector3 _moveInput;
+    Vector2 input;
+    Vector3 moveVelocity;
+    Vector3 moveInput;
 
-    bool _jump;
+    bool jump;
     bool canJump;
 
-    Rigidbody _Rigidbody;
+    Rigidbody rigidBody;
     Camera mainCamera;
     Ray lookRay;
 
     // Start is called before the first frame update
     void Start()
     {
-        _Rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _Input.x = Input.GetAxis("Horizontal");
-        _Input.y = Input.GetAxis("Vertical");
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            _jump = true;
+            jump = true;
         }
         else
         {
-            _jump = false;
+            jump = false;
         }
 
-        _moveInput = new Vector3(_Input.x, 0, _Input.y);
+        moveInput = new Vector3(input.x, 0, input.y);
         //_moveInput.Normalize();
-        _moveVelocity = transform.forward * _movespeed * _moveInput.sqrMagnitude;
+        moveVelocity = transform.forward * moveSpeed * moveInput.sqrMagnitude;
 
         Vector3 CameraForward = mainCamera.transform.forward;
         CameraForward.y = 0;
 
         Quaternion CameraRelativeRotation = Quaternion.FromToRotation(Vector3.forward, CameraForward);
 
-        Vector3 looktoward = CameraRelativeRotation * _moveInput;
+        Vector3 looktoward = CameraRelativeRotation * moveInput;
 
-        if (_moveInput.sqrMagnitude > 0)
+        if (moveInput.sqrMagnitude > 0)
         {
             lookRay = new Ray(transform.position, looktoward);
             Vector3 dir = lookRay.GetPoint(1);
@@ -63,13 +63,13 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_jump)
+        if (jump)
         {
-            _Rigidbody.AddForce(Vector3.up * _JumpForce);
-            _jump = false;
+            rigidBody.AddForce(Vector3.up * jumpForce);
+            jump = false;
         }
 
-        _moveVelocity.y = _Rigidbody.velocity.y;
-        _Rigidbody.velocity = _moveVelocity;
+        moveVelocity.y = rigidBody.velocity.y;
+        rigidBody.velocity = moveVelocity;
     }
 }
