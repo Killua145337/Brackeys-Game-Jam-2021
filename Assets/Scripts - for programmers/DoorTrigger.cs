df7c37950d61354d72cd58cc970e934c5eccaa0f
Killua145337 <68349080+Killua145337@users.noreply.github.com>
2021-02-15 16:53:10 +0330

    Added Pressure Plate

---
+++ Assets/Scripts - for programmers/DoorTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    [SerializeField] float doorPositionChange;
+
+    private Vector3 startingPos;
+
+
+    private void Start() {
+        startingPos = door.transform.position;
+    }
+    private void OnTriggerStay(Collider other) {
+        door.transform.position += new Vector3(0f, doorPositionChange, 0f) * Time.deltaTime;
+        GetComponent<Animator>().SetBool("triggered", true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        door.transform.position = startingPos;
+        GetComponent<Animator>().SetBool("triggered", false);
+    }
+}
