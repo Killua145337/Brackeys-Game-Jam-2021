260c7b0b187fa849359bf6a3e54ebb559b60e63a
venkatesh21m <46576190+venkatesh21m@users.noreply.github.com>
2021-02-15 17:45:49 +0530

    With loading panel, main menu

---
+++ Assets/Scripts - for programmers/LevelManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    #region parameters
+    [SerializeField] LoadingPanel loadingpanel;
+    private string _curentLevelName;
+    #endregion
+
+    #region SceneLoading
+    public void LoadLevel(string levelName)
+    {
+        _curentLevelName = levelName;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
+        ao.completed += OnLoadOperationComplete;
+    }
+
+    private void OnLoadOperationComplete(AsyncOperation obj)
+    {
+        Debug.Log("Loding " + _curentLevelName + "completed");
+        loadingpanel.DoOutAnimation();
+    }
+    #endregion
+
+    #region button functions
+    public void StartGame(string levelname)
+    {
+        loadingpanel.DoInAnimation(levelname);
+    }
+    public void ReStart()
+    {
+        loadingpanel.DoInAnimation(SceneManager.GetActiveScene().name);
+    }
+    public void GOHome()
+    {
+        loadingpanel.DoInAnimation("Boot");
+    }
+
+    public void StartSceneLoading(string sceneName)
+    {
+        loadingpanel.DoInAnimation(sceneName);
+    }
+    #endregion
+}
---
+++ Assets/Scripts - for programmers/LoadingPanel.cs
@@ -0,0 +1,63 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingPanel : MonoBehaviour
+{
+    string LevelName;
+    [SerializeField] Image[] images;
+    [SerializeField] TMPro.TMP_Text[] texts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DoOutAnimation", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DoInAnimation(string LevelName)
+    {
+        this.LevelName = LevelName;
+        foreach (var item in images)
+        {
+            item.DOFade(1, 1);
+        }
+
+        foreach (var item in texts)
+        {
+            item.DOFade(1, 1);
+        }
+
+        Invoke("OnInAnimationEnd", 1.1f);
+    }
+
+    public void OnInAnimationEnd()
+    {
+        LevelManager.instance.LoadLevel(LevelName);
+    }
+
+    public void DoOutAnimation()
+    {
+        foreach (var item in images)
+        {
+            item.DOFade(0, 1);
+        }
+
+        foreach (var item in texts)
+        {
+            item.DOFade(0, 1);
+        }
+    }
+
+    public void OnOutAnimationEnd()
+    {
+
+    }
+}
