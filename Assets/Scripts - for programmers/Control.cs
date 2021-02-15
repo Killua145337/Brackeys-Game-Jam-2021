17d966c6d5a427da4392fdac99267df150c8d39f
Killua145337 <68349080+Killua145337@users.noreply.github.com>
2021-02-15 14:53:32 +0330

    howling

---
+++ Assets/Scripts - for programmers/Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField] AudioClip howlSound;
+    [SerializeField] float howlTime;
+
+
+    Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetBool("howl", true);
+            AudioSource.PlayClipAtPoint(howlSound, transform.position);
+            StartCoroutine(StopHowlAnimation());
+        }
+
+        
+    }
+
+    IEnumerator StopHowlAnimation()
+    {
+        yield return new WaitForSeconds(howlTime);
+        animator.SetBool("howl", false);
+    }
+}
