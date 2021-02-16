967aa283f19b2281c5b67446078ef4002eccf673
speedhs <64911433+speedhs@users.noreply.github.com>
2021-02-16 06:14:00 +0530

    added the first level+few assets
    bababoey and gamedev you can start simlutaneously making artistic and mechanical changes.

---
+++ Assets/Scripts - for programmers/newteleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newteleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+public Transform teleportTarget;
+public GameObject thePlayer;
+
+void OnTriggerEnter(Collider other)
+{
+    thePlayer.transform.position = teleportTarget.transform.position;
+}
+
+}
