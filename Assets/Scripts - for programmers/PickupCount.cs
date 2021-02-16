34a3c7614e81c528dcae73bc8bd6e119edc4f535
Killua145337 <68349080+Killua145337@users.noreply.github.com>
2021-02-16 21:50:06 +0330

    Pickup system and stars

---
+++ Assets/Scripts - for programmers/PickupCount.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickupCount : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI starText;
+    public int starsCollected = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        starText.text = "Stars Collected: " + starsCollected.ToString();
+    }
+}
